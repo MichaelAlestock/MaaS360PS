c61ee923f5e29ec43977ebafed34b0e2af92d270
Anthony Alestock <m.alestock93@gmail.com>
2025-10-04 18:19:22 -0400

    feat: changed formatting
    Changed the parameters to being inside of the function along with your standard cmdletbinding attritbute for future debugging needs.

--- source/private/Get-ProgressInformation.ps1
+++ source/private/Get-ProgressInformation.ps1
@@ -1,4 +1,11 @@
-function Get-ProgressInformation ([int]$Count, [int]$Page, [int]$Size)
+function Get-ProgressInformation
 {
+    [CmdletBinding()] # Just in case we need it for debugging purposes in the future
+    param(
+        [int]$Count,
+        [int]$Page,
+        [int]$Size
+    )
+    
     Write-Information -MessageData "Total Returned Objects: $Count | Page Number: $Page | Page Size: $Size" -InformationAction 'Continue'
 }
